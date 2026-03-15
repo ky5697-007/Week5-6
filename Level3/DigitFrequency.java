0122d3c658f739a4f90dc4dd1e9d93075985d973
ky5697 -007 <ky5697@srmist.edu.in>
2026-03-15 12:41:17 +0530

    Koushik added Digit Frequency to git

---
+++ Level3/DigitFrequency.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class DigitFrequency{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int temp = number;
+        int count = 0;
+
+        while (temp != 0) {
+            temp = temp / 10;
+            count++;
+        }
+
+        int[] digits = new int[count];
+        int index = 0;
+
+        while (number != 0) {
+            digits[index] = number % 10;
+            number = number / 10;
+            index++;
+        }
+
+        int[] frequency = new int[10];
+
+        for (int i = 0; i < digits.length; i++) {
+            frequency[digits[i]]++;
+        }
+
+        System.out.println("Digit Frequency:");
+
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] > 0) {
+                System.out.println("Digit " + i + " occurs " + frequency[i] + " times");
+            }
+        }
+
+        sc.close();
+    }
+}
